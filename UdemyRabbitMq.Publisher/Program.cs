51e4205ae5c0dc1cd58937d892e54673a907f3e9
iknow <iknow@DESKTOP-8D9SJL3>
2024-09-26 14:07:02 +0300

    Add project files.

---
+++ UdemyRabbitMq.Publisher/Program.cs
@@ -0,0 +1,31 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Text;
+
+namespace UdemyRabbitMq.Publisher
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory();
+            factory.Uri = new Uri("amqps://mgdeodss:4fD2_W6XNSgK4xRHtYVm_HSRoFcPyRrd@chimpanzee.rmq.cloudamqp.com/mgdeodss");
+
+            using var connection = factory.CreateConnection();
+
+            var channel = connection.CreateModel();
+
+            channel.QueueDeclare("hello-queue",true,false,false);
+
+            string message = "hello world";
+
+            var messageBody = Encoding.UTF8.GetBytes(message);
+
+            channel.BasicPublish(string.Empty,"hello-queue",null,messageBody);
+
+            Console.WriteLine("Mesaj Gönderilmiştir");
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ UdemyRabbitMq.Subscriber/Program.cs
@@ -0,0 +1,37 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Reflection;
+using System.Text;
+
+namespace UdemyRabbitMq.Subscriber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory();
+            factory.Uri = new Uri("amqps://mgdeodss:4fD2_W6XNSgK4xRHtYVm_HSRoFcPyRrd@chimpanzee.rmq.cloudamqp.com/mgdeodss");
+
+            using var connection = factory.CreateConnection();
+
+            var channel = connection.CreateModel();
+
+           // channel.QueueDeclare("hello-queue", true, false, false);
+
+            var consumer = new EventingBasicConsumer(channel);
+
+            channel.BasicConsume("hello-queue", true, consumer);
+
+            consumer.Received += (object sender, BasicDeliverEventArgs e) =>
+            {
+                var message = Encoding.UTF8.GetString(e.Body.ToArray());
+                Console.WriteLine("Gelen Mesaj" + message);
+            };
+
+            Console.ReadLine();
+        }
+
+    
+    }
+}
